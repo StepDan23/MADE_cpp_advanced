4b197827bf8c0cacb92ec840a1fba6a28f2a65aa
StepDan23 <mix2ra93@mail.ru>
2020-09-26 10:54:54 +0300

    add docker with ubuntu arm compiler add main.s with count_det function in assembler lang

--- count_det/src/main.s
+++ count_det/src/main.s
@@ -1,2 +1,38 @@
-/// Your code goes here...
+.text
+.global count_det
 
+count_det:
+        ldrsb r1, [r0], #4
+        ldrsb r2, [r0], #4
+        ldrsb r3, [r0], #4
+        ldrsb r4, [r0], #4
+        ldrsb r5, [r0], #4
+        ldrsb r6, [r0], #4
+        ldrsb r7, [r0], #4
+        ldrsb r8, [r0], #4
+        ldrsb r9, [r0], #4
+
+        mul r0, r1, r5
+        mul r0, r9
+
+        mul r10, r2, r6
+        mul r10, r7
+        add r0, r10
+
+        mul r10, r3, r4
+        mul r10, r8
+        add r0, r10
+
+        mul r1, r6
+        mul r1, r8
+        sub r0, r1
+
+        mul r2, r4
+        mul r2, r9
+        sub r0, r2
+
+        mul r3, r5
+        mul r3, r7
+        sub r0, r3
+
+        bx lr
